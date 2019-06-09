b6786ae97e2b05bf6732a14046b86b073ee5404c
GMMULLER <39245943+GMMULLER@users.noreply.github.com>
2019-06-08 22:52:14 -0300

    Add files via upload

---
+++ sac-test.php
@@ -0,0 +1,22 @@
+<?php
+
+	use PHPUnit\Framework\TestCase;
+	
+	class SacTest extends TestCase{
+
+		public function testGetTicket(){
+
+			$appHost = $_ENV["ET_SUT_HOST"];
+		
+			if($appHost == NULL){
+				$appHost = "localhost";
+			}
+
+			$response = file_get_contents("http://$appHost:5000/api/sac");
+			$this->assertNull($response);
+
+		}	
+	}
+
+?>
+
