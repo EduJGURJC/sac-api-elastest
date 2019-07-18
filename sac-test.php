a832bd3c0e5c800a6efb925716cc32558b06a5bd
GMMULLER <39245943+GMMULLER@users.noreply.github.com>
2019-07-18 19:38:00 -0300

    Update sac-test.php

--- sac-test.php
+++ sac-test.php
@@ -12,7 +12,15 @@
 				$appHost = "localhost";
 			}
 
-			$response = file_get_contents("http://$appHost:80/tickets");
+			$curl_handle=curl_init();
+			curl_setopt($curl_handle, CURLOPT_URL,"http://$appHost:80/tickets");
+			curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+			curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+			$query = curl_exec($curl_handle);
+			curl_close($curl_handle);
+			
+			//$response = file_get_contents("http://$appHost:80/tickets");
 			$this->assertNull(NULL);
 
 		}	
