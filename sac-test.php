a291cc2e09a5881d1da5f9687f18fb71d1f1b889
GMMULLER <39245943+GMMULLER@users.noreply.github.com>
2019-07-19 13:28:14 -0300

    Update sac-test.php

--- sac-test.php
+++ sac-test.php
@@ -20,7 +20,7 @@
 			//$query = curl_exec($curl_handle);
 			//curl_close($curl_handle);
 			
-			$response = file_get_contents("http://$appHost:80");
+			$response = file_get_contents("http://$appHost:80/");
 			$this->assertNull(NULL);
 			//fwrite(STDERR, print_r($query, TRUE));
 			//$this->assertJsonStringEqualsJsonString(
